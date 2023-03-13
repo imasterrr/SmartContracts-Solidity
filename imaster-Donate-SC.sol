cc5eeac85d3c8a918d24f4561986ef6fdf575efb
masterweb3 <86267357+imasterrr@users.noreply.github.com>
2023-03-13 23:21:17 +0400

    Create imaster-Donate-SC.sol

---
+++ imaster-Donate-SC.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+
+contract ImasterDonate {
+
+    address public owner;
+    mapping (address => uint) public payments;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function Donate() public payable {
+        payments[msg.sender] = msg.value;
+    }
+
+    function Return() public {
+        address payable _to = payable(owner);
+        address _thisContract = address(this);
+        _to.transfer(_thisContract.balance);
+    }
+}
